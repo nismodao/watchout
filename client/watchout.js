db8b9d05ed9d4abe8a435d289b8b69766b381bfd
Nam Dao <nam.d.dao@gmail.com>
2016-04-30 10:26:58 -0700

    Completed collision detection

--- client/watchout.js
+++ client/watchout.js
@@ -8,7 +8,7 @@ svg.attr('class',"container")
    .style('background-color',"steelblue");
 
 
-var enemyProp = [4, 8, 15, 22, 55];
+var enemyProp = [4,5,6,7,8,9];
 // var enemy = d3.select('svg').append('circle')
 //    .attr('cx', Math.random() * 700)
 //    .attr('cy', Math.random() * 700)
@@ -28,12 +28,15 @@ var createEnemies = d3.select('svg').selectAll('circle').data(enemyProp)
 
 function makeItMove() {
   d3.selectAll('.enemy')
-  .transition().duration(2000)
+    .transition().duration(2000)
     .attr('cx', function() { return Math.random() * 1000; })
     .attr('cy', function() { return Math.random() * 1000; })
-    .call(collide)
-    .each('end', function() { makeItMove(); });
+    //.call(collide) -- causes repeats
+    .each('end', function() { makeItMove(); });   
 }
+
+
+
 
 
 function onDragDrop(dragHandler){
@@ -64,73 +67,29 @@ function dragmove(d){
 }
 
 
-// function Collision (node1,node2) {
 
-
-//   this.x1 = x1;
-//   this.y1 = y1;
-//   this.x2 = x2;
-//   this.y2 = y2;
-//   this.radius1 = radius1;
-//   this.radius2 = radius2;
-// }
-
-// Collision.prototype.collision = function (node1,node2) {
-// if (Math.sqrt( (this.x2-this.x1)) * (this.x2-this.x1) + (this.y2-this.y1) * (this.y2-this.y1) < (this.radius1 + this.radius2)) {
-//   return true;
-//   //collison detected do something;
-// }
-
-// }
-// function collide() {
-//   node = this.node();
-//   nodeBox = node.getBBox();
-//   nodeLeft = nodeBox.x;
-//   nodeRight = nodeBox.x + nodeBox.width;
-//   nodeTop = nodeBox.y;
-//   nodeBottom = nodeBox.y + nodeBox.height;
-
-//   d3.selectAll('.enemy')
-//     .style('fill', function(){
-//       if(this !== node) {
-//         otherBox = this.getBBox();
-//         otherLeft = otherBox.x;
-//         otherRight = otherBox.x + otherBox.width;
-//         otherTop = otherBox.y;
-//         otherBottom = otherBox.y + otherBox.height;
-
-//         collideHoriz = nodeLeft < otherRight && nodeRight > otherLeft;
-//         collideVert = nodeTop < otherBottom && nodeBottom > otherTop;
-
-//         if(collideHoriz && collideVert) {
-//           count++;
-//           console.log("red");
-//           return 'red';
-//         } else {
-//           return 'blue';
-//         }
-//       } else {
-//         return "blue";
-//       }
-//     });
-// }
-var collide = function (enemy, collidedCallback) {
-  var radiusSum = parseFloat(enemy.attr('r')) + player.r;
-  var xDiff = parseFloat(enemy.attr('cx')) - player.x;
-  var yDiff = parseFloat(enemy.attr('cy')) - player.y;
-
-  var separation = Math.sqrt( Math.pow(xDiff,2) + Math.pow(yDiff,2) );
-  if (separation < radiusSum) {
-    console.log("collison detect hopefully");
-  collidedCallback(player, enemy); 
-  }
+var count = 0;
+function collide () {
+  var player = d3.select('.player');
+  var r = player.attr('r');
+  var x = player.attr('cx');
+  var y = player.attr('cy');
+  var current
+  d3.selectAll('.enemy').each(function(d,i) {
+    var enemy = d3.select(this);
+    var radiusSum = parseFloat(enemy.attr('r')) + parseInt(r);
+    var xDiff = parseFloat(enemy.attr('cx')) - parseInt(x);
+    var yDiff = parseFloat(enemy.attr('cy')) - parseInt(y);
+    var separation = Math.sqrt( Math.pow(xDiff,2) + Math.pow(yDiff,2) ); 
+      if (separation < radiusSum) {
+        count++;
+        console.log('collision detected with', d, 'seperation is', separation, 'radius is',radiusSum);
+        console.log(count);
+    }
+  });
 }
 
-d3.selectAll('.enemy').each(function(d,i) {
-  console.log('d is', d, "i is", i);
- //collide(value);
-
-});
+setInterval(collide,700);
 
 
 
